5217a53e788e8505f0f2b0b6a1ff35463704bb3b
CharlieTung <choelestung@gmail.com>
2018-10-29 17:20:18 -0400

    Initial commit
    initial commit

---
+++ bookstore/app.js
@@ -0,0 +1,55 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+var app = express()
+Genre = require('./models/genre')
+Book = require('./models/book')
+
+//connect to mongoose
+mongoose.connect("mongodb://localhost/bookstore")
+var db = mongoose.connection 
+
+app.get('/', function(req, res){
+    res.send("Please use /api/books or /api/genres")
+})
+
+app.get('/api/genres', function(req, res){
+    Genre.getGenres(function(err, genres) {
+        if (err) {
+            throw err
+        }
+        res.json(genres)
+    })
+})
+app.post('/api/genres', function(req, res){
+    var genre = req.body
+    Genre.addGenres(genre, function(err, genre) {
+        if (err) {
+            throw err
+        }
+        res.json(genre)
+    })
+})
+
+
+app.get('/api/books', function(req, res){
+    Book.getBooks(function(err, books) {
+        if (err) {
+            throw err
+        }
+        res.json(books)
+    })
+})
+app.get('/api/books/:_id', function(req, res){
+    Book.getBookById(req.params._id, function(err, book) {
+        if (err) {
+            throw err
+        }
+        res.json(book)
+    })
+})
+
+app.listen(3000)
+console.log('Running on port 3000...');
+
+
